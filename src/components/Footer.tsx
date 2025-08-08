c9ac128182c53846d497e3ffafa37a7373cf151e
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-08-08 11:32:20 -0300

    ajuste no readme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,12 +26,11 @@ export const Footer = () => {
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
+            { /* Usage of this template requires attribution. Please don't remove the link to Once UI. */}            
             <SmartLink
               href="https://once-ui.com/products/magic-portfolio"
             >
-              Once UI
+              Magic Portfolio
             </SmartLink>
           </Text>
         </Text>
